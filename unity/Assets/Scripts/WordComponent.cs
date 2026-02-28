4c1c823cc1bf43e70b0fcf04ebcd07edf9349135
Anton123xx <115159228+Anton123xx@users.noreply.github.com>
2026-02-28 16:33:39 -0500

    dsfs
    fsefsfs

--- unity/Assets/Scripts/WordComponent.cs
+++ unity/Assets/Scripts/WordComponent.cs
@@ -2,15 +2,14 @@ using UnityEngine;
 
 public class WordComponent : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public string word = "OPEN";
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+           // PlayerController player = other.GetComponent<PlayerController>();
+            //player.PickUpWord(word, gameObject);
+        }
     }
 }
